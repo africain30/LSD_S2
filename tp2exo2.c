5a03f2192feba6d7efaef6aefef23a94d48343c3
africain30 <80362737+africain30@users.noreply.github.com>
2021-03-25 18:31:00 +0100

    Update tp2exo2.c

--- tp2exo2.c
+++ tp2exo2.c
@@ -110,8 +110,30 @@ t_liste removenode( int n , t_liste L )
 
 }
 
-
-
+int main(){
+    t_list c;
+    t_list* k=&c;
+    int n,T[n],m;
+    printf("Enter the desired array length: ");
+    scanf("%d",&n);
+    if(n>0){
+        printf("Please start typing the array values:\n");
+        for(int i=0;i<n;i++){
+            scanf("%d",&T[i]);
+        }
+        c=arraytoliste(T,n);
+        printlist(c);
+        printf("Remove a node by typing it int address: ");
+        scanf("%d",&m);
+        removenode(k,m);
+    }
+    else{
+        c=arraytoliste(T,n);
+    }
+    printliste(c);
+    freenode(c);
+    return 0;
+}
 
 
 
