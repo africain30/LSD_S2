9d3982ac2dde993e8f5e6e15f1531e5a2fbcff8a
africain30 <80362737+africain30@users.noreply.github.com>
2021-03-25 18:39:00 +0100

    Update tp2exo2.c

--- tp2exo2.c
+++ tp2exo2.c
@@ -1,16 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-//declaration des fonctions a utiliser 
 
+//la declaration de la nouvelle stricture de donnnes
 struct node
 {
     int element  ;
     struct node * next ;
 
 };
-typedef struct node node;
+typedef struct node node; // pour modifier son nom d'appelle dans les autre fonction
 typedef node* t_liste;
+
+
+
+//declaration des fonctions a utiliser 
+	t_liste creat_node(int); //pour creer une nouvelle cellule
+	t_liste add_node(int  , t_liste); //ajouter une celloule qu'on vient de creer a une liste chainees
+	t_liste arraytoliste(int* , int ); //pour convertir un tableau a une liste 
+	void printliste(t_liste ); //pour afficher la liste
+	t_liste removenode( int , t_liste) //pour pouvoire supprimer une cellule 
+	void free_node(t_list L); //pour liberer la zone memoire reserveé lors de la creation de la cellule
+
+
 
 t_liste creat_node(int element  )
 {
@@ -109,6 +121,20 @@ t_liste removenode( int n , t_liste L )
     }
 
 }
+
+
+void free_node(t_list L){
+    while(L!=NULL){
+        t_list p=L;
+        L=L->next;
+        free(p);
+    }
+    return ;
+}
+
+
+
+
 
 int main(){
     t_list c;
