45d0124d135588acc17910d1c67a0c36dde15795
africain30 <80362737+africain30@users.noreply.github.com>
2021-06-08 09:38:44 +0100

    Create main.c

---
+++ TP1/EXERCISE1/main.c
@@ -0,0 +1,22 @@
+include "functions.h"
+include "functions.c"
+
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  // int result = recfib(n);
+  // printf("%d\n" , result);
+  // int result2 = fibo(n);
+  // printf("%d\n" , result2);
+  // int result3 = isprime(n);
+  // printf("%d\n" , result3);
+  // getprime(n);
+  int result5 = isprime(n);
+  printf("%d\n" , result5);
+  return 0;
+}
+
+    © 2021 GitHub, Inc.
+    Terms
