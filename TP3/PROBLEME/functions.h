3c2ad6e4b1c6b2882f0f056b242fc3a4b3113657
africain30 <80362737+africain30@users.noreply.github.com>
2021-06-10 15:57:30 +0100

    Update functions.h

--- TP3/PROBLEME/functions.h
+++ TP3/PROBLEME/functions.h
@@ -1,1 +1,17 @@
-HHHHH
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+typedef struct stack
+{
+	int data;
+	struct stack *next;
+} stack;
+
+
+void push( stack **, int  );
+int pop( stack ** );
+int eval( char , stack** );
+int need( char  );
+int checknr( char*  );
