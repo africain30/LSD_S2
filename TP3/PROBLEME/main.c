d61ea1e9524c4df8d8869f05d8a2c1ad56cfc716
africain30 <80362737+africain30@users.noreply.github.com>
2021-06-10 15:58:38 +0100

    Update main.c

--- TP3/PROBLEME/main.c
+++ TP3/PROBLEME/main.c
@@ -1,4 +1,54 @@
-int main()
+#include "functions.h"
+
+
+
+
+int main( int argc, char** argv )
 {
-  return 0;
+	int i, temp, stacksize = 0;
+	stack* head = NULL;
+
+	if( argc == 1 )
+	{
+		printf("Usage: %s <arg> [arg] ... \n", argv[0]);
+		exit(1);
+	}
+
+	for( i = 1; i < argc; i++ ) {
+		char* token = argv[i];
+		char* endptr;
+		char op;
+
+		if( checknr( token ) ) {
+			/* We have a valid number. */
+			temp = atoi( token );
+			push( &head, temp );
+			++stacksize;
+		} else {
+			/* We have an operand (hopefully) */
+			if( strlen( token ) != 1 ) {
+				fprintf( stderr, "Error: Token '%s' too large.\n", token );
+				exit( 1 );
+			}
+
+			op = token[0];
+
+			if( stacksize < need( op ) ) {
+				fputs( "Too few arguments on stack.\n", stderr );
+				exit( 1 );
+			}
+
+			push( &head, eval( op, &head ) );
+			stacksize -= need( op ) - 1;
+		}
+	}
+
+	if( stacksize != 1 ) {
+		fputs( "Too many arguments on stack.\n", stderr );
+		exit( 1 );
+	}
+
+	printf( "Result: %i\n", head->data );
+
+	return 0;
 }
